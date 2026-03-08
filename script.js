c46d319a20960288adb3be82c38ef9de6a1cbdc2
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-03-08 23:24:22 +0530

    added css file

--- script.js
+++ script.js
@@ -1,72 +1,24 @@
-const input = document.getElementById("commandInput");
-const output = document.getElementById("output");
+const spellBtn = document.getElementById("spellBtn");
+const book = document.getElementById("book");
+const voldemort = document.getElementById("voldemort");
 
-const commands = {
+spellBtn.onclick = () => {
 
-help: `
-Available Commands
+book.style.display = "flex";
 
-about     - About me
-skills    - My tech stack
-projects  - My projects
-contact   - Contact info
-clear     - Clear terminal
-`,
-
-about: `
-Hi I am Yuvraj
-MCA Student
-Frontend Developer
-
-Specializing in
-React
-JavaScript
-Web Development
-`,
-
-skills: `
-HTML
-CSS
-JavaScript
-React
-Git
-`,
-
-projects: `
-1. Online Code Editor
-2. Typing Speed Tester
-3. Delivery Date Predictor
-4. Fitness Tracking System
-`,
-
-contact: `
-Email : yuvrajbhagyedh@gmail.com
-Phone : 8088713395
-GitHub : github.com/yuvraj
-LinkedIn : linkedin.com
-`
+spellBtn.innerText = "Magic Revealed";
 
 };
 
-input.addEventListener("keypress", function(e){
 
-if(e.key === "Enter"){
+document.addEventListener("mousemove", () => {
 
-let cmd = input.value.trim();
+if(Math.random() > 0.98){
+voldemort.style.display = "block";
 
-output.innerHTML += `<div class="output-line"><span class="prompt">yuvraj@linux:~$</span> ${cmd}</div>`;
-
-if(cmd === "clear"){
-output.innerHTML = "";
-}
-else if(commands[cmd]){
-output.innerHTML += `<div class="output-line">${commands[cmd]}</div>`;
-}
-else{
-output.innerHTML += `<div class="output-line">Command not found</div>`;
-}
-
-input.value = "";
+setTimeout(()=>{
+voldemort.style.display = "none";
+},2000)
 
 }
 
